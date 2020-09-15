a1c668a323efc96b8acdad6229c1c29afe89fa21
Philipp Schuster <phip1611@gmail.com>
2020-09-15 11:57:44 +0200

    get input from readline and transform it to std::string without lost memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
+// C++ Includes
 #include <iostream>
 
+// C includes
+#include <readline/history.h>
+#include <readline/readline.h>
+
+// Own includes
 #include "lib.hpp"
 
-int main(void) {
-	std::cout << "Hallo Welt" << std::endl;
-	std::cout << say_hello() << std::endl;
-	return 0;   
+
+int main() {
+    while(true) {
+        char * input = readline("$ ");
+        // creates a deep copy; string has a constructor for char *
+        std::string input_str = input;
+        free(input);
+
+        // std::cout << "Input is: '" << input << "'" << std::endl;
+
+
+    }
+	return 0;
 }
